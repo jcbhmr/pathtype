39337d47513d645f345a66c7c6ebf08f42fd5f70
Jacob Hummer <jcbhmr@outlook.com>
2026-03-07 04:06:09 +0000

    save

--- filepathtype/pattern.go
+++ filepathtype/pattern.go
@@ -1,12 +1,54 @@
 package filepathtype
 
 import (
+	"iter"
 	"path/filepath"
 	"unsafe"
 )
 
 type Pattern struct {
 	inner string
+}
+
+func (p Pattern) String() string {
+	return p.inner
+}
+
+func (p Pattern) Bytes() []byte {
+	return []byte(p.inner)
+}
+
+func (p Pattern) Runes() []rune {
+	return []rune(p.inner)
+}
+
+func (p Pattern) RunesSeq() iter.Seq2[int, rune] {
+	return func(yield func(int, rune) bool) {
+		for i, r := range p.inner {
+			if !yield(i, r) {
+				return
+			}
+		}
+	}
+}
+
+func (p Pattern) At(i int) byte {
+	return p.inner[i]
+}
+
+// Compare compares p and o according to their inner strings.
+//
+//   - p < o: -1
+//   - p == o: 0
+//   - p > o: 1
+func (p Pattern) Compare(o Pattern) int {
+	if p.inner < o.inner {
+		return -1
+	} else if p.inner > o.inner {
+		return 1
+	} else {
+		return 0
+	}
 }
 
 func NewPattern(pattern string) (Pattern, error) {
